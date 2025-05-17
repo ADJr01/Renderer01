d9e6fd2b75d74d20ca0972d85efbfb4769a64724
rahat.adnan <sthep90@Gmail.com>
2025-05-17 12:23:38 +0600

    Game Loop created

--- Renderer01/main.cpp
+++ Renderer01/main.cpp
@@ -2,17 +2,30 @@
 #include<SDL.h>
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
-bool is_created = false;
+bool SDL_INIT_FLAG = false;
+// function declarations
 bool init_sdl();
+void setup();
+void process_input();
+void update();
+void render();
+void cleanup();
 
 int main(int argc,char* args[]) {
-	is_created = init_sdl();
-	if (!is_created)
+	SDL_INIT_FLAG = init_sdl();
+	if (!SDL_INIT_FLAG)
 	{
 		std::cerr << "Failed to initialize SDL system."<<SDL_GetError() << std::endl;
 		return 1;
 	}
 	std::cout<<"Initialized SDL"<<std::endl;
+	setup();
+	while (SDL_INIT_FLAG){
+		process_input();
+		//if (!SDL_INIT_FLAG)return 0;
+		update();
+		render();
+	}
 	return 0;
 }
 
@@ -47,4 +60,26 @@ bool init_sdl() {
 		return false;
 	}
 	return true;
+}
+void setup(){}
+void process_input()
+{
+	SDL_Event event;
+	SDL_PollEvent(&event);
+	if (event.type == SDL_QUIT){
+		SDL_INIT_FLAG = false;
+	}else if (event.type == SDL_KEYDOWN){
+		 const auto key = event.key.keysym.sym;
+		if (key==SDLK_ESCAPE){
+			SDL_INIT_FLAG = false;
+		}
+	}
+}
+void update(){
+
+}
+void render(){
+	SDL_SetRenderDrawColor(renderer,105,195,125,190);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
 }
